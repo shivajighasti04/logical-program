c77b4f30f11184117db46e79f8215b9521fa3863
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-11 12:31:30 +0530

    ADDED: Binary To Decimal conversion Program

---
+++ BinaryToDecimal.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.junitprograms;
+
+/**
+ * Binary to decimal conversion program
+ * 
+ * @author Shivaji Ghasti
+ *
+ */
+public class BinaryToDecimal {
+	public static int toDecimal(int binary) {
+	
+		int baseValue = 1, decimal = 0;
+		int number = binary;
+		int temporary = number;
+
+		while (temporary > 0) {
+			int lastDigit = temporary % 10;
+			temporary = temporary / 10;
+			decimal += lastDigit * baseValue;
+			baseValue *= 2;
+		}
+		return decimal;
+	}
+
+	public static void main(String[] args) {
+		int binaryNumber = 101;
+		int decimalRepresenatation = toDecimal(binaryNumber);
+		System.out.println("The Decimal representation of " + binaryNumber + " is : " + decimalRepresenatation);
+	}
+}
